5f36a999284cf64befd884d549dcb829ddec918e
Zeki Korkmaz <redrose19592001@gmail.com>
2021-08-01 23:10:07 +0300

    ilk sürüm

---
+++ src/package1/ClassOne.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class ClassOne {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Batch 35 / 36 learn Github");
+    }
+}
+
