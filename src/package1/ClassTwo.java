528ade7f82394d59228d14ced67c419f6dfb300d
Zeki Korkmaz <redrose19592001@gmail.com>
2021-08-07 21:29:51 +0300

    We go on with second file

---
+++ src/package1/ClassTwo.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class ClassTwo {
+    public static void main(String[] args) {
+        System.out.println("Hello World twice");
+        int x=5;
+        int sum = x+2;
+        System.out.println(sum);
+    }
+}
